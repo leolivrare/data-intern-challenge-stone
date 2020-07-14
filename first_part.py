9386a6bd8aa518224304e2cf980af9136356a632
leolivrare <l220120@dac.unicamp.br>
2020-07-14 19:28:48 -0300

    Improvements in card_family_limit_analisys

--- first_part.py
+++ first_part.py
@@ -7,10 +7,18 @@ def customers_average_age(db):
 
 def card_family_limit_analisys(db):
     df_cards = pd.DataFrame(db.select_table("cards"))
+
     group_average = df_cards[[1, 2]].groupby(1)
     group_average.mean().columns = ["Average Limit"]
+
     print("The average of credit limits of each card family is:")
     print(group_average.mean())
+
+    print("The maximum credit limit of each card family is:")
+    print(group_average.max())
+
+    print("The minimum credit limit of each card family is:")
+    print(group_average.min())
 
 
 def highest_value_fraud_id(db):
